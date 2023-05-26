294e20034b09fe0ca6991756d06a12f8ba2b1982
fury <107589431+1337fury@users.noreply.github.com>
2023-05-26 15:21:11 +0100

    Update ft_lstdelone.c
    remove main function

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -12,40 +12,10 @@
 
 #include "libft.h"
 
-void	del(void *ptr)
-{
-	free(ptr);
-}
-
 void	ft_lstdelone(t_list *lst, void (*del)(void*))
 {
 	if (!lst || !del)
 		return ;
 	del(lst->content);
 	free(lst);
-}
-
-#include <stdio.h>
-int main ()
-{
-	t_list *s1 = malloc(sizeof(t_list));
-	t_list *s2 = malloc(sizeof(t_list));
-	t_list *s3 = malloc(sizeof(t_list));
-
-	s1->content = "hello";	
-	s1->next = s2;	
-	
-	s2->content = "world";	
-	s2->next = s3;
-	
-	s3->content = "i am the last node";
-	s3->next = NULL;
-
-
-	t_list *new = malloc(sizeof(t_list));
-	new->content = "i am the new node";
-
-	ft_lstdelone(s2, del);
-	
-	printf("%s", s3->content);	
 }
